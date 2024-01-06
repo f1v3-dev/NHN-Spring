4e69863208a8247d77a94e4714fce5dc9fbfc67e
f1v3-dev <f1v3@kakao.com>
2024-01-06 21:03:46 +0900

    Exam: 과정 결산 프로젝트 1 - springframework 마이그레이션 #1

--- springframework-project1/src/main/java/com/nhnacademy/edu/springframework/project/repository/Student.java
+++ springframework-project1/src/main/java/com/nhnacademy/edu/springframework/project/repository/Student.java
@@ -1,12 +1,19 @@
 package com.nhnacademy.edu.springframework.project.repository;
 
 
+import java.util.Objects;
+
 public class Student {
     private final int seq;
     private final String name;
     private Score score;
 
     public Student(int seq, String name) {
+
+        if (name.isEmpty() || Objects.isNull(name)) {
+            throw new IllegalArgumentException("name is invalid!");
+        }
+
         this.seq = seq;
         this.name = name;
     }
@@ -24,6 +31,11 @@ public class Student {
     }
 
     public void setScore(Score score) {
+
+        if (Objects.isNull(score)) {
+            throw new IllegalArgumentException("Score is invalid !!");
+        }
+
         this.score = score;
     }
 
